bc268057d286c1851bdc7705fd5e1aea1966e0af
Joao Gilberto Magalhaes <joao@byjg.com.br>
2014-04-27 22:27:35 -0300

    Adjust in Chart Sample

--- lib/Lesson1/Modules/Sample6.class.php
+++ lib/Lesson1/Modules/Sample6.class.php
@@ -49,6 +49,8 @@ class Sample6 extends BaseModule
 		$chart->setTitleHorizontal('Title Horizontal');
 		$chart->setTitleVertical('Title Vertical');
 		//$chart->setIs3d(true);
+		$chart->setMinValueY(0);
+		
 		$chart->addSerie('Year2', \Xmlnuke\Core\Enum\ChartColumnType::String, [2004, 2005, 2006, 2007]);
 		$chart->addSerie('Sales2', \Xmlnuke\Core\Enum\ChartColumnType::Number, [1000, 1170, 660, 1030]);
 		$chart->addSerie('Expenses2', \Xmlnuke\Core\Enum\ChartColumnType::Number, [400, 460, 1120, 540]);
